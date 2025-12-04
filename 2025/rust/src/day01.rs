6be9db28051cfce0e80a803d11d9ff8b0beb7b37
yasincaykur <yasincaykur@gmail.com>
2025-12-04 03:30:56 +0300

    add day1 rust

--- 2025/rust/src/day01.rs
+++ 2025/rust/src/day01.rs
@@ -1,7 +1,71 @@
-pub fn part1(input: &str) -> String {
-    "TODO".to_string()
+pub fn part1(input: &str) -> i32 {
+    let mut part1_password: i32 = 0;
+    let dial_size: i32 = 100;
+    let mut current_pos: i32 = 50;
+    let s: Vec<&str> = parse_input(input);
+    for c in s {
+        let direction: &str = &c[..1];
+        let distance: i32 = c[1..].parse().unwrap();
+
+        if direction == "R" {
+            current_pos = (current_pos + distance) % dial_size;
+        } else  {
+            current_pos = (current_pos - distance) % dial_size;
+        }
+        if current_pos == 0 {
+            part1_password += 1;
+        }
+
+    }
+    part1_password
 }
 
-pub fn part2(input: &str) -> String {
-    "TODO".to_string()
+pub fn parse_input(input: &str) -> Vec<&str> {
+    input.trim().split('\n').collect()
+}
+
+
+pub fn part2(input: &str) -> i32 {
+    let dial_size: i32 = 100;
+    
+    let mut current_pos: i32 = 50;
+    let mut part2_password: i32 = 0;
+    
+    let s: Vec<&str> = parse_input(input);
+
+    for c in s {
+        let direction: &str = &c[..1];
+        let distance: i32 = c[1..].parse().unwrap();
+
+        let start_pos: i32 = current_pos;
+        
+        let mut hits_during_rotation: i32 = 0;
+        
+        if direction == "R" {
+            hits_during_rotation = (start_pos + distance) / dial_size;
+
+        } else if direction == "L" {
+            if start_pos == 0 {
+                hits_during_rotation = distance / dial_size;
+            }   
+            else {
+                if distance >= start_pos {
+                    hits_during_rotation = ((distance - start_pos) / dial_size) + 1;
+                }       
+                else {
+                    hits_during_rotation = 0;
+                }       
+            }       
+        }
+        part2_password += hits_during_rotation;
+
+        if direction == "R" {
+            current_pos = (current_pos + distance).rem_euclid(dial_size);
+        }
+        else if direction == "L" {
+            current_pos = (current_pos - distance).rem_euclid(dial_size);
+        }   
+    }   
+        
+    part2_password
 }
--- 2025/rust/src/main.rs
+++ 2025/rust/src/main.rs
@@ -14,13 +14,13 @@ fn main() {
     }
 
     let day = &args[1];
-    let input_path = format!("../inputs/day{}.txt", day);
+    let input_path = format!("../inputs/day0{}.txt", day);
     let input = fs::read_to_string(&input_path)
         .unwrap_or_else(|_| panic!("Could not read {}", input_path));
 
     match day.as_str() {
-        "01" => println!("Part 1: {}\nPart 2: {}", day01::part1(&input), day01::part2(&input)),
-        "02" => println!("Part 1: {}\nPart 2: {}", day02::part1(&input), day02::part2(&input)),
+        "1" => println!("Part 1: {}\nPart 2: {}", day01::part1(&input), day01::part2(&input)),
+        "2" => println!("Part 1: {}\nPart 2: {}", day02::part1(&input), day02::part2(&input)),
         // "XX" => println!("Part 1: {}\nPart 2: {}", dayXX::part1(&input), dayXX::part2(&input)),
         _ => eprintln!("Day {} not implemented", day),
     }
